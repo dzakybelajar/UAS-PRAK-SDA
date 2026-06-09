a00541dc25973e70458d9f76818c8efa5e604d3d
dzakybelajar <dzakysyraj@mhs.usk.ac.id>
2026-06-10 00:29:31 +0700

    penambahan tampilan menu utama

--- main.c
+++ main.c
@@ -4,6 +4,16 @@
 
 void menuUtama() {
     int pilihan;
+    while (1) {
+        pilihan = 0;
+        printf("\n=== MEDTRACK PRO ===\n");
+        printf("1. Masuk Sebagai Resepsionis\n");
+        printf("2. Masuk Sebagai Dokter\n");
+        printf("3. Keluar Aplikasi\n");
+        printf("Pilih Akses: "); 
+        scanf("%d", &pilihan);
+
+    }
 }
 
 int main() {
