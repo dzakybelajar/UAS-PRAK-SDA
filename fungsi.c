6ab26e7b01e07057bc6b1028c1cb54a540869cd3
mhddrayyan <mhdrayyan50@gmail.com>
2026-06-09 23:37:56 +0700

    menambahkan fungsi mergesort

--- fungsi.c
+++ fungsi.c
@@ -281,3 +281,12 @@ void merge(PasienNode** arr, int l, int m, int r) {
     while (j < n2) arr[k++] = R[j++];
     free(L); free(R);
 }
+
+void mergeSort(PasienNode** arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m + 1, r); 
+        merge(arr, l, m, r);
+    }
+}
